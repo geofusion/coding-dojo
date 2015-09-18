a02deb566f813e557055e2e4f4f3c9d88a717b4a
Alexandre Queiroz <alexandre.queiroz@geofusion.com.br>
2015-09-18 07:37:25 -0300

    Preparando coding-dojo

--- geocoding-services/src/main/java/br/com/geofusion/service/GeocodingService.java
+++
@@ -1,8 +0,0 @@
-package br.com.geofusion.service;
-
-import javax.ws.rs.core.Response;
-
-public interface GeocodingService {
-	public Response reverse(final Double longitude, final Double latitude);
-	public Response geocode(final String streetName, final Integer houseNumber, final String neighborhood, final String municipality, final String state, final String postalCode, final String country);
-}
